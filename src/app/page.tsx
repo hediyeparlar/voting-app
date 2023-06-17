bce71bd3607f8c92c52b15f58b2dab3dd2d91468
hediyeparlar <hediye.parlar@hotmail.com>
2023-06-17 11:46:33 +0300

    home page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import HomePage from "../pages/Home/HomePage";
 
@@ -6,16 +5,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <HomePage />
-      {/* 
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-     */}
     </main>
   );
 }
